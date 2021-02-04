f1f455e8b6f45cf58179180af4febd0743d68024
Kirti2198 <59197640+Kirti2198@users.noreply.github.com>
2021-02-04 21:14:26 +0530

    changed styling

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/User_List',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(`mongodb://localhost/User_List`);
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_youtube', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
 
 db.once('open', function(){
     console.log('Connected to Database :: MongoDB');
